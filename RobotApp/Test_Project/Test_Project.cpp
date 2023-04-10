f1f9174857406ffc0125d0aa55376f550aa13261
pcl1203 <lagasca.pc@gmail.com>
2023-04-10 16:39:27 +0800

    added unit test for the movement class

--- RobotApp/Test_Project/Test_Project.cpp
+++ RobotApp/Test_Project/Test_Project.cpp
@@ -3,19 +3,26 @@
 #include "CppUnitTest.h"
 #include "../RobotApp/Entity.cpp"
 #include "../RobotApp/Platform.cpp"
+#include "../RobotApp/Movement.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestProject
 {
+	Platform Table;
 	TEST_CLASS(ENTITY)
 	{
 	public:
+
+		TEST_CLASS_INITIALIZE(ClassInitialize)
+		{
+			Table.SetSize(5, 5);
+		}
 		#pragma region STATUS
 		TEST_METHOD(GetOrientation)
 		{
 			Entity robot(0, 1, FacingOrientation::NORTH);
-			Assert::AreEqual(int(FacingOrientation::NORTH), int(robot.GetOrientation()));
+			Assert::AreEqual(std::string("NORTH"), robot.GetOrientation());
 		}
 
 		TEST_METHOD(GetPosX)
@@ -36,13 +43,15 @@ namespace TestProject
 		TEST_METHOD(RotateRight)
 		{
 			Entity robot(0, 1, FacingOrientation::NORTH);
-			Assert::AreEqual(int(FacingOrientation::EAST), int(robot.RotateRight()));
+			robot.RotateRight();
+			Assert::AreEqual(std::string("EAST"), robot.GetOrientation());
 		}
 
 		TEST_METHOD(RotateLeft)
 		{
 			Entity robot(2, 1, FacingOrientation::EAST);
-			Assert::AreEqual(int(FacingOrientation::NORTH), int(robot.RotateLeft()));
+			robot.RotateLeft();
+			Assert::AreEqual(std::string("NORTH"), robot.GetOrientation());
 		}
 		#pragma endregion
 
@@ -51,9 +60,9 @@ namespace TestProject
 		TEST_METHOD(Move_X_FWD)
 		{
 			Entity robot(0, 3, FacingOrientation::EAST);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(1), robot.GetPosX());
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(2), robot.GetPosX());
 			Assert::AreEqual(unsigned char(3), robot.GetPosY());
 		}
@@ -61,91 +70,91 @@ namespace TestProject
 		TEST_METHOD(Move_X_BWD)
 		{
 			Entity robot(3, 3, FacingOrientation::WEST);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(2), robot.GetPosX());
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(1), robot.GetPosX());
 		}
 
 		TEST_METHOD(Move_X_FWD_BWD)
 		{
 			Entity robot(3, 3, FacingOrientation::EAST);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(4), robot.GetPosX());
 			robot.RotateLeft();
 			robot.RotateLeft();
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(3), robot.GetPosX());
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(2), robot.GetPosX());
 			robot.RotateLeft();
 			robot.RotateLeft();
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(3), robot.GetPosX());
 		}
 
 		TEST_METHOD(Move_X_Limit_Max)
 		{
 			Entity robot(0xff, 3, FacingOrientation::EAST);
-			robot.Move();
-			Assert::AreEqual(unsigned char(0), robot.GetPosX());
+			robot.Move(Table);
+			Assert::AreEqual(unsigned char(0xff), robot.GetPosX());
 		}
 
 		TEST_METHOD(Move_X_Limit_Min)
 		{
 			Entity robot(0, 3, FacingOrientation::WEST);
-			robot.Move();
-			Assert::AreEqual(unsigned char(0xff), robot.GetPosX());
+			robot.Move(Table);
+			Assert::AreEqual(unsigned char(0), robot.GetPosX());
 		}
 
 		TEST_METHOD(Move_Y_FWD)
 		{
 			Entity robot(0, 3, FacingOrientation::NORTH);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(4), robot.GetPosY());
-			robot.Move();
-			Assert::AreEqual(unsigned char(5), robot.GetPosY());
+			robot.Move(Table); // exceeds
+			Assert::AreEqual(unsigned char(4), robot.GetPosY());
 			Assert::AreEqual(unsigned char(0), robot.GetPosX());
 		}
 
 		TEST_METHOD(Move_Y_BWD)
 		{
 			Entity robot(3, 3, FacingOrientation::SOUTH);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(2), robot.GetPosY());
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(1), robot.GetPosY());
 		}
 
 		TEST_METHOD(Move_Y_FWD_BWD)
 		{
 			Entity robot(3, 3, FacingOrientation::NORTH);
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(4), robot.GetPosY());
 			robot.RotateLeft();
 			robot.RotateLeft();
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(3), robot.GetPosY());
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(2), robot.GetPosY());
 			robot.RotateLeft();
 			robot.RotateLeft();
-			robot.Move();
+			robot.Move(Table);
 			Assert::AreEqual(unsigned char(3), robot.GetPosY());
 		}
 
 		TEST_METHOD(Move_Y_Limit_Max)
 		{
 			Entity robot(0xff, 0xff, FacingOrientation::NORTH);
-			robot.Move();
-			Assert::AreEqual(unsigned char(0), robot.GetPosY());
+			robot.Move(Table);
+			Assert::AreEqual(unsigned char(0xff), robot.GetPosY());
 		}
 
 		TEST_METHOD(Move_Y_Limit_Min)
 		{
 			Entity robot(0, 0, FacingOrientation::SOUTH);
-			robot.Move();
-			Assert::AreEqual(unsigned char(0xff), robot.GetPosY());
+			robot.Move(Table);
+			Assert::AreEqual(unsigned char(0), robot.GetPosY());
 		}
 		#pragma endregion
 
@@ -189,6 +198,102 @@ namespace TestProject
 			table.GetSize(uTilesX, uTilesY);
 			Assert::AreEqual(unsigned char(0), uTilesX);
 			Assert::AreEqual(unsigned char(0), uTilesY);
+		}
+	};
+	TEST_CLASS(MOVEMENT)
+	{
+
+		TEST_CLASS_INITIALIZE(ClassInitialize)
+		{
+			Table.SetSize(5, 5);
+		}
+		TEST_METHOD(ExtractOrientation_NORTH)
+		{
+			Movement test;
+			auto ptr_moves = test.ExtractOrientation("NORTH");
+			Assert::AreEqual(std::string("NORTH"), ptr_moves->faced);
+
+			auto ptr_ccw = ptr_moves->ccw;
+			Assert::AreEqual(std::string("WEST"), ptr_ccw->faced);
+
+			auto ptr_cw = ptr_moves->cw;
+			Assert::AreEqual(std::string("EAST"), ptr_cw->faced);
+
+			Coords coords(0, 0);
+			ptr_moves->fwd(&coords, Table);
+			Assert::AreEqual((uint8_t)0, coords.x);
+			Assert::AreEqual((uint8_t)1, coords.y);
+
+			Coords coords2(0, 4);
+			ptr_moves->fwd(&coords2, Table);
+			Assert::AreEqual((uint8_t)0, coords2.x);
+			Assert::AreEqual((uint8_t)4, coords2.y); // no movement due to table has only 5 tiles
+		}
+		TEST_METHOD(ExtractOrientation_SOUTH)
+		{
+			Movement test;
+			auto ptr_moves = test.ExtractOrientation("SOUTH");
+			Assert::AreEqual(std::string("SOUTH"), ptr_moves->faced);
+
+			auto ptr_ccw = ptr_moves->ccw;
+			Assert::AreEqual(std::string("EAST"), ptr_ccw->faced);
+
+			auto ptr_cw = ptr_moves->cw;
+			Assert::AreEqual(std::string("WEST"), ptr_cw->faced);
+
+			Coords coords(0, 0);
+			ptr_moves->fwd(&coords, Table);
+			Assert::AreEqual((uint8_t)0, coords.x);
+			Assert::AreEqual((uint8_t)0, coords.y); // no movement
+
+			Coords coords2(1, 4);
+			ptr_moves->fwd(&coords2, Table);
+			Assert::AreEqual((uint8_t)1, coords2.x);
+			Assert::AreEqual((uint8_t)3, coords2.y);
+		}
+		TEST_METHOD(ExtractOrientation_EAST)
+		{
+			Movement test;
+			auto ptr_moves = test.ExtractOrientation("EAST");
+			Assert::AreEqual(std::string("EAST"), ptr_moves->faced);
+
+			auto ptr_ccw = ptr_moves->ccw;
+			Assert::AreEqual(std::string("NORTH"), ptr_ccw->faced);
+
+			auto ptr_cw = ptr_moves->cw;
+			Assert::AreEqual(std::string("SOUTH"), ptr_cw->faced);
+
+			Coords coords(0, 0);
+			ptr_moves->fwd(&coords, Table);
+			Assert::AreEqual((uint8_t)1, coords.x);
+			Assert::AreEqual((uint8_t)0, coords.y); 
+
+			Coords coords2(4, 3);
+			ptr_moves->fwd(&coords2, Table);
+			Assert::AreEqual((uint8_t)4, coords2.x);// no movement
+			Assert::AreEqual((uint8_t)3, coords2.y);
+		}
+		TEST_METHOD(ExtractOrientation_WEST)
+		{
+			Movement test;
+			auto ptr_moves = test.ExtractOrientation("WEST");
+			Assert::AreEqual(std::string("WEST"), ptr_moves->faced);
+
+			auto ptr_ccw = ptr_moves->ccw;
+			Assert::AreEqual(std::string("SOUTH"), ptr_ccw->faced);
+
+			auto ptr_cw = ptr_moves->cw;
+			Assert::AreEqual(std::string("NORTH"), ptr_cw->faced);
+
+			Coords coords(0, 0);
+			ptr_moves->fwd(&coords, Table);
+			Assert::AreEqual((uint8_t)0, coords.x);// no movement
+			Assert::AreEqual((uint8_t)0, coords.y);
+
+			Coords coords2(4, 3);
+			ptr_moves->fwd(&coords2, Table);
+			Assert::AreEqual((uint8_t)3, coords2.x);
+			Assert::AreEqual((uint8_t)3, coords2.y);
 		}
 	};
 }
